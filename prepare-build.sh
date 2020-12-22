927853520565306db62ec0a770cdc591bc8dc947
Mathias Bogaert <mathias.bogaert@gmail.com>
2020-12-22 18:35:21 +0000

    Upgrades.

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p target/dependency/intellij-idea
-curl -L https://download.jetbrains.com/idea/ideaIC-2017.2.5.tar.gz | tar xz --strip-components=1 -C target/dependency/intellij-idea
+curl -L https://download.jetbrains.com/idea/ideaIC-2020.3.tar.gz | tar xz --strip-components=1 -C target/dependency/intellij-idea
